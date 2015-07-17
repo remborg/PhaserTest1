9f6d7faaec3ab49beed5e99200981274472c9c68
Remi Borgniet <remi.borgniet@viacesi.fr>
2015-07-17 07:45:31 +0100

    Phaser lib and gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,42 @@
 var gulp = require('gulp');
-
-
 var sass = require('gulp-ruby-sass');
+var livereload = require('gulp-livereload');
 
 
 var sass = require('gulp-ruby-sass');
 gulp.task('sass', function() {
     return sass('src/scss/style.scss', {style: 'compressed'})
-        .pipe(gulp.dest('build/css/'));
+        .pipe(gulp.dest('build/css/'))
+        .pipe(livereload());
 });
 
 gulp.task('copyJs', function() {
     return gulp.src('src/js/**/*.js')
-        .pipe(gulp.dest('build/js/'));
+        .pipe(gulp.dest('build/js/'))
+        .pipe(livereload());
 });
 gulp.task('copyHtml', function() {
-    return gulp.src('*.html')
-        .pipe(gulp.dest('.'));
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build/'))
+        .pipe(livereload());
 });
 gulp.task('copyImg', function() {
     return gulp.src(['src/img/**/*.png', 'src/img/**/*.jpg'])
-        .pipe(gulp.dest('build/img/'));
+        .pipe(gulp.dest('build/img/'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function() {
+  livereload.listen();
   // Watch .js files
   gulp.watch('src/js/*.js', ['copyJs']);
   // Watch .scss files
   gulp.watch('src/scss/*.scss', ['sass']);
 
-  gulp.watch('*.html', ['copyHtml']);
+  gulp.watch('src/*.html', ['copyHtml']);
   // Watch image files
   gulp.watch(['src/img/**/*.png', 'src/img/**/*.jpg'], ['copyImg']);
 });
-
-
 
 
 gulp.task('default', ['sass', 'copyJs', 'copyHtml', 'copyImg', 'watch']);
